bb3e692ba351a1b49d48e2ec0fb79633bfcfec8d
alser15 <mar233@mail.ru>
2021-10-13 12:09:34 +0300

    Create test

--- sand_box/BasePage/RegistrationPage.py
+++ sand_box/BasePage/RegistrationPage.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from sand_box.BasePage.BasePage import BasePage
-
+import allure
 
 class RegistrationPage(BasePage):
     def __init__(self, init_mobile_device):
@@ -65,26 +65,31 @@ class RegistrationPage(BasePage):
             (By.XPATH,
                 "//android.widget.FrameLayout[@resource-id='biz.growapp.winline:id/vgSmsCodeField']/android.widget.FrameLayout/android.widget.TextView[@resource-id='biz.growapp.winline:id/tvReg']")))
 
+    @allure.step('Клик на банер')
     def click_on_banner(self):
         """ Клик на банер"""
         self.advertising_banner().click()
         return self
 
+    @allure.step('Клик на поля ввода телефона')
     def click_input_phone(self):
         """ Поиск поля ввода телефона"""
         self.input_phone().click()
         return self
 
+    @allure.step('Ввод номера телефона')
     def enter_phone(self, number_phone):
         """ Ввод номера телефона"""
         self.input_phone().send_keys(number_phone)
         return self
 
+    @allure.step('Клик на форму Дата Рождения')
     def click_input_Birthday(self):
         """ Раскрытие формы дня рождения"""
         self.input_Birthday().click()
         return self
 
+    @allure.step('Ввод месяца в форму Дата Рождения')
     def enter_moth(self, moth):
         """ Ввод месяца в форму дня рождения """
         self.input_Birthday_moth().click()
@@ -92,6 +97,7 @@ class RegistrationPage(BasePage):
         self.input_Birthday_moth().set_value(moth)
         return self
 
+    @allure.step('Ввод дня в форму Дата Рождения')
     def enter_day(self, day):
         """ Ввод дня в форму дня рождения """
         self.input_Birthday_day().click()
@@ -99,6 +105,7 @@ class RegistrationPage(BasePage):
         self.input_Birthday_day().set_value(day)
         return self
 
+    @allure.step('Ввод года в форму Дата Рождения')
     def enter_year(self, year):
         """ Ввод года в форму дня рождения"""
         self.input_Birthday_year().click()
@@ -106,51 +113,61 @@ class RegistrationPage(BasePage):
         self.input_Birthday_year().set_value(year)
         return self
 
+    @allure.step('Клик на кнопку cancel в форму Дата Рождения')
     def click_button_cancel(self):
         """ Нажать копку cancel в форме выбора даты рождения"""
         self.button_cancel_Birthday().click()
         return self
 
+    @allure.step('Клик на кнопку ОК в форму Дата Рождения')
     def click_button_ok(self):
         """ Нажать копку OK в форме выбора даты рождения"""
         self.button_OK_Birthday().click()
         return self
 
+    @allure.step('Ввод пароля')
     def enter_password(self, password):
         """ Ввод пароля"""
         self.input_Password().send_keys(password)
         return self
 
+    @allure.step('Клик на надпись "У меня есть промокод"')
     def click_on_promo(self):
         """ Клик по надписи У меня есть промокод """
         self.promo().click()
         return self
 
+    @allure.step('Клик на поле с промокодом')
     def click_promo_input(self):
         """ Клик по полю с промокодом """
         self.promo_input().click()
         return self
 
+    @allure.step('Ввод промокода')
     def enter_promo(self, promocode):
         """ Ввод промокода """
         self.promo_input_enter().send_keys(promocode)
         return self
 
+    @allure.step('Клик на кнопку "Соглашение с правилами"')
     def tap_on_switch_button(self):
         """ Клик по соглашению с правилами """
         self.switch_button().click()
         return self
 
+    @allure.step('Клик на кнопку регистрации')
     def click_button_registration(self):
         """ клик на кнопку регистрации"""
         self.button_registration().click()
         return self
 
+    @allure.step('Ввод СМС кода')
     def enter_sms_code(self, sms_code):
         """ Ввод СМС кода """
         self.input_sms_code().send_keys(sms_code)
         return self
 
+    @allure.step('Клик на кнопку продолжить')
     def click_button_next(self):
         """ Клик на кнопку продолжить """
         self.button_next().click()
--- sand_box/conftest.py
+++ sand_box/conftest.py
@@ -1,5 +1,7 @@
 import pytest
 from appium import webdriver
+import allure
+import os
 
 
 @pytest.fixture(scope='function')
@@ -16,3 +18,26 @@ def init_mobile_device():
     yield driver
     driver.close_app()
     driver.quit()
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    """ Скрин при падении теста """
+    outcome = yield
+    rep = outcome.get_result()
+    if rep.when == 'call' and rep.failed:
+        mode = 'a' if os.path.exists('failures') else 'w'
+        try:
+            with open('failures', mode) as f:
+                if 'init_mobile_device' in item.fixturenames:
+                    driver = item.funcargs['init_mobile_device']
+                else:
+                    print('Fail to take screen-shot')
+                    return
+            allure.attach(
+                driver.get_screenshot_as_png(),
+                name='error',
+                attachment_type=allure.attachment_type.PNG
+            )
+        except Exception as e:
+            print('Fail to take screen-shot: {}'.format(e))
